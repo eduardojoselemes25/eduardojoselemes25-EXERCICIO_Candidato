13872410abd8692c63c78019055e5430e7126176
eduardojoselemes25 <50559973+eduardojoselemes25@users.noreply.github.com>
2020-05-14 21:51:20 -0300

    Leitura dos outros dados

--- src/aplicacao/programa.java
+++ src/aplicacao/programa.java
@@ -18,6 +18,20 @@ public class programa {
 		System.out.println("3) Ensino Superior");
 		System.out.println("4) Pós-graduação");
 		System.out.println("Digite uma opção:2 ");
+        System.out.println();
+
+		
+		System.out.println("Você tem quantos anos de experiência profissional? 3");
+		System.out.println("Você tem disponibilidade para viajar (s/n)? n");
+		System.out.println("Você tem habilitação de motorista (s/n)? s");
+		System.out.println();
+		
+		System.out.println("Pontos por escolaridade: 20");
+		System.out.println("Pontos por experiência: 15");
+		System.out.println();
+		
+		System.out.println("Você está habilitado para o(s) seguintes(s) cargo(o): ");
+		System.out.println("ASSISTENTE");
 		
 	}
 }
