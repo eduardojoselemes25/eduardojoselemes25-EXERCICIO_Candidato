7d3d3662acf7f02ed5236123697600dc811089b9
eduardojoselemes25 <50559973+eduardojoselemes25@users.noreply.github.com>
2020-05-14 21:47:50 -0300

    Leitura da Escolaridade

---
+++ src/aplicacao/programa.java
@@ -0,0 +1,23 @@
+package aplicacao;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class programa {
+
+	public static void main(String[] args) {
+	
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
+	
+		System.out.println("Qual sua escolaridade?");
+		System.out.println("1) Ensino Fundamental");
+		System.out.println("2) Ensino Médio");
+		System.out.println("3) Ensino Superior");
+		System.out.println("4) Pós-graduação");
+		System.out.println("Digite uma opção:2 ");
+		
+	}
+}
